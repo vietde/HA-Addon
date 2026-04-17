998b53a740dd27030fb49bf15364ef32f4ae32cd
Vietde <chetdi65@gmail.com>
2026-04-17 10:31:35 -0700

    Update without no browser

--- 9Router/run.sh
+++ 9Router/run.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/with-contenv bashio
-
-bashio::log.info "Starting 9Router..."
-
-# Find the npm global bin directory and run from there
-export PATH=$PATH:$(npm config get prefix)/bin
-
-9router --host 0.0.0.0 --port 20128 --no-browser
+echo "Starting 9Router..."
+9router --host 0.0.0.0 --port 20128
