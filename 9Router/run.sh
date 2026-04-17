558c007ccee2a875cea82d80157fedbd8313826b
Tai Bui <taibui01@outlook.com>
2026-04-17 10:20:09 -0700

    Update run.sh

--- 9Router/run.sh
+++ 9Router/run.sh
@@ -4,4 +4,4 @@ bashio::log.info "Starting 9Router..."
 
 # Start 9Router on the default port 20128
 # --host 0.0.0.0 is critical for container networking
-9router --host localhost --port 20128 --no-browser
+9router --host 0.0.0.0 --port 20128 --no-browser
